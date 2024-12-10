7ed2a9aa0ecbbf940792d5f69f1d114c438c06dd
Hiroyasu <ankousabit@gmail.com>
2024-12-09 16:38:32 -0800

    Added comments on plus_network.R

--- script/plus_network.R
+++ script/plus_network.R
@@ -1,46 +1,58 @@
-# read the libraries 
+# Library
 library(igraph)
 
-setwd("~/Desktop/stergm-small-multiple-networks/")
+#################### Functions ####################
 
-# load the data: pgg_data
-load("data/pgg_data.RData")
-
-# calculate all the plus graphs
+# Calculate all the plus graphs from a network
+# net: an igraph object
 cal_plus_graphs <- function(net) {
   
+  # Number of nodes
   node_num <- vcount(net)
   
+  # All the vertex pairs: 15 pairs
   all_vertex_pairs <- t(combn(node_num, 2))
   
+  # Check if the vertex pairs are connected
   bool_connected <- apply(all_vertex_pairs, 1, function(pair) {
       igraph::are.connected(net, pair[1], pair[2])})
   
+  # Non-connected pairs
   non_connected_pairs <- all_vertex_pairs[! bool_connected, ]
   
+  # All the combinations of non-connected pairs
   combinations <- expand.grid(
     rep(list(c(FALSE, TRUE)), nrow(non_connected_pairs)))
   
+  # A list for all the plus graphs
   plus_graphs_list <- list()
   
-  if (nrow(combinations) == 0) {
+  # If there is no non-connected pairs
+  if (nrow(combinations) == 1) {
     
+    # The plus graph is the same as the input network
     graph_plus_list[[1]] <- net
     
   } else {
     
+    # Generate all the plus graphs
     for (j in 1:nrow(combinations)) {
       
+      # Copy the input network
       temp_graph <- net
       
+      # Delete the edges based on the combinations
       for (k in 1:nrow(non_connected_pairs)) {
         
+        # If the combination is TRUE
         if (combinations[j, k]) {
           
+          # Add the edge
           temp_graph <- add_edges(temp_graph, non_connected_pairs[k, ])
           
         }}
       
+      # Add the plus graph to the list
       plus_graphs_list[[j]] <- temp_graph
       
     }
@@ -51,15 +63,22 @@ cal_plus_graphs <- function(net) {
 
 }
 
-# calculate the plus graphs in the dynamic network
+# Calculate the plus graphs in a set of dynamic networks (7)
+# nets: a set of dynamic networks
 cal_plus_dynamic <- function(nets) {
   
+  # Number of time steps -1 : 7
+  # Note that there are 8 networks in "nets", but calculate the plus graphs for 7 networks.
   time_step <- length(nets) - 1
+  # A list for all the plus graphs
   plus_dynamic_list <- list()
   
+  # Calculate the plus graphs for each time step
   for (t in 1:time_step) {
     
+    # Calculate the plus graphs
     plus_graphs_list <- cal_plus_graphs(nets[[t]])
+    # Save the plus graphs
     plus_dynamic_list[[t]] <- plus_graphs_list
     
   }
@@ -68,15 +87,20 @@ cal_plus_dynamic <- function(nets) {
   
 }
 
-# calculate the plus graphs of all the dynamic networks
+# Calculate the plus graphs for all the 20 dynamic networks
+# net: a list of 20 dynamic networks
 cal_plus_set <- function(net) {
   
+  # A list for all the plus graphs
   plus_set_list <- list()
+  # Number of networks: 20
   network_num <- length(net)
   
   for (n in 1:network_num) {
     
+    # Calculate the plus graphs for each dynamic network
     plus_dynamic_list <- cal_plus_dynamic(net[[paste0("net_", n)]])
+    # Save the plus graphs
     plus_set_list[[paste0("net_", n)]] <- plus_dynamic_list
     
   }
@@ -85,10 +109,18 @@ cal_plus_set <- function(net) {
   
 }
 
-# run the function
+#################### Run ####################
+
+# Set the working directory
+setwd("~/Desktop/stergm-small-multiple-networks/")
+
+# load the data: pgg_data
+load("data/pgg_data.RData")
+
+# Run the function
 pgg_plus_data <- cal_plus_set(pgg_data)
 
-
+# Convert the plus graphs to adjacency matrices
 pgg_plus_adj <- lapply(pgg_plus_data, function(sublist) {
   
   lapply(sublist, function(subsublist) {
@@ -100,6 +132,6 @@ pgg_plus_adj <- lapply(pgg_plus_data, function(sublist) {
   })
 
 
-# save the data
+# Save the data
 save(pgg_plus_data, file = "data/pgg_plus_data.RData")
 save(pgg_plus_adj, file = "data/pgg_plus_adj.RData")
